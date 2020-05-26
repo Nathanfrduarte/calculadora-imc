87db94e8e1514b5fd08675f0a615bf827f536fcd
Nathan Duarte <nathan.duarte51.nd@gmail.com>
2020-05-26 00:34:48 -0300

    Implementação inicial da calculadora

---
+++ javascript/imc.js
@@ -0,0 +1,51 @@
+import CalcularIMC from './CalcularIMC.js'
+
+class IMC {
+
+    //Como não queremos que as properties sejam manipuladas externamente
+    //Fornecemos GETTERS somente leitura para nossas variáveis
+    get peso() {
+        return this._peso;
+    }
+
+    get altura() {
+        return this._altura;
+    }
+
+    get imc() {
+        return this._imc;
+    }
+
+    get classificacao() {
+        return this._classificacao;
+    }
+
+    get consequencia() {
+        return this._consequencia;
+    }
+
+    get data() {
+        return this._data;
+    }
+
+    constructor(peso, altura) {
+        this._peso = peso
+        this._altura = altura
+        this._data = new Date()
+    }
+
+    calcular() {
+        //Chamada pra função CalcularIMC
+        const { imc, classificacao, consequencia } = CalcularIMC(
+            this.peso,
+            this.altura
+        );
+        
+        //Atribuímos valores a properts do objeto.
+        this._imc = imc;
+        this._classificacao = classificacao;
+        this._consequencia = consequencia;
+    }
+}
+
+export default IMC
